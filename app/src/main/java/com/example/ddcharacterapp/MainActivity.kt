5fbf09aef9c7abc1e07d170969f95ed29a10b0e9
Sawyer Knowles <sdknowle@mtu.edu>
2023-02-10 15:31:27 -0800

    Refactored adding new characters to use dialog instead of activity

--- app/src/main/java/com/example/ddcharacterapp/MainActivity.kt
+++ app/src/main/java/com/example/ddcharacterapp/MainActivity.kt
@@ -1,29 +1,49 @@
 package com.example.ddcharacterapp
 
-import android.app.Activity
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.View
 import android.widget.LinearLayout
 import android.widget.TextView
-import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult
 import androidx.cardview.widget.CardView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnInputListener {
+    private fun addNewCharacterToList(newName: String, newClass: String, newLevel: String) {
+        Log.d("Check2", "Activity addNewCharacterToList Called")
+        val charLayout = findViewById<LinearLayout>(R.id.charLayout)
+        val newCharView = layoutInflater.inflate(R.layout.list_item_character, null)
+
+        val menuName = newCharView.findViewById<TextView>(R.id.mName)
+        val menuClass = newCharView.findViewById<TextView>(R.id.mMenuClass)
+        val menuLevel = newCharView.findViewById<TextView>(R.id.mMenuLevel)
+        menuName.text = newName
+        menuClass.text = newClass
+        menuLevel.text = newLevel
+
+        val characterCard = newCharView.findViewById<CardView>(R.id.characterCard)
+        characterCard.setOnClickListener() {
+            Log.d("CharacterName", "Character Name: " + menuName.text.toString())
+            Log.d("CharacterClass", "Character Class: " + menuClass.text.toString())
+            Log.d("CharacterLevel", "Character Level: " + menuLevel.text.toString())
+        }
+
+        charLayout.addView(newCharView)
+    }
+
+    override fun sendNewCharacterInfo(newName: String, newClass: String, newLevel: String) {
+        Log.d("Check", "Activity sendNewCharacterInfo Called")
+        addNewCharacterToList(newName, newClass, newLevel)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val addCharButton = findViewById<FloatingActionButton>(R.id.floatingActionButton)
-        val charLayout = findViewById<LinearLayout>(R.id.charLayout)
 
-        //val newCharView = layoutInflater.inflate(R.layout.list_item_character, null)
-        //charLayout.addView(newCharView)
-
+        /* Get Character Info Using Activity
         //Launch InputCharacterInfo activity for results
         var resultLauncher = registerForActivityResult(StartActivityForResult()) { result ->
             if (result.resultCode == Activity.RESULT_OK) {
@@ -50,11 +70,16 @@ class MainActivity : AppCompatActivity() {
 
                 charLayout.addView(newCharView)
             }
-        }
+        }*/
+
         addCharButton.setOnClickListener() {
+            var dialog = NewCharacterDialogFragment()
+            dialog.show(supportFragmentManager, "newCharacterDialog")
+
+            /* Start Character Info View Using Activity
             val charInfoPop = Intent(this, InputCharacterInfo::class.java)
             resultLauncher.launch(charInfoPop)
-            //startActivity(charInfoPop)
+            //startActivity(charInfoPop)*/
         }
     }
 }
---
+++ app/src/main/java/com/example/ddcharacterapp/newCharacterDialogFragment.kt
@@ -0,0 +1,55 @@
+package com.example.ddcharacterapp
+
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.fragment.app.DialogFragment
+
+interface OnInputListener {
+    fun sendNewCharacterInfo(newName: String, newClass: String, newLevel: String)
+}
+
+class NewCharacterDialogFragment: DialogFragment() {
+
+    private lateinit var inputListener: OnInputListener
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        var rootView: View = inflater.inflate(R.layout.new_character_dialog, container, false)
+
+        val cancelButton = rootView.findViewById<Button>(R.id.cancel_button)
+        cancelButton.setOnClickListener() {
+            dismiss()
+        }
+
+        val addButton = rootView.findViewById<Button>(R.id.add_button)
+        addButton.setOnClickListener() {
+            val newName = rootView.findViewById<TextView>(R.id.new_character_name).text.toString()
+            val newClass = rootView.findViewById<TextView>(R.id.new_character_class).text.toString()
+            val newLevel = rootView.findViewById<TextView>(R.id.new_character_level).text.toString()
+            inputListener.sendNewCharacterInfo(newName, newClass, newLevel)
+            dismiss()
+        }
+
+        return rootView
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+
+        try {
+            inputListener = activity as OnInputListener
+        } catch (e: ClassCastException) {
+            Log.d("ClassCastExceptionDetected", "Activity doesn't implement the onInputListener interface")
+        }
+    }
+}
