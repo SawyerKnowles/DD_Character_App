1a07b6d95f714f0f260f8bac79560814427f1621
Sawyer Knowles <sdknowle@mtu.edu>
2023-03-08 12:55:42 -0800

    Implemented data persistence for traits data

--- app/src/main/java/com/example/ddcharacterapp/TraitsFragment.kt
+++ app/src/main/java/com/example/ddcharacterapp/TraitsFragment.kt
@@ -2,28 +2,39 @@ package com.example.ddcharacterapp
 
 import android.app.Activity
 import android.content.Intent
-import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import android.widget.ImageView
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.ui.graphics.RectangleShape
+import androidx.core.net.toUri
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
 import com.canhub.cropper.CropImageContract
 import com.canhub.cropper.CropImageView
 import com.canhub.cropper.*
-import java.net.URI
 
 class TraitsFragment : Fragment() {
 
     private var characterImage : Uri? = null
+    var name = ""
+    var characterClass = ""
+    var level = ""
+    var race = ""
+    var alignment = ""
+    var personality = ""
+    var ideals = ""
+    var bonds = ""
+    var flaws = ""
+    var background = ""
 
     private val cropImage = registerForActivityResult(CropImageContract()) { result ->
         if (result.isSuccessful) {
@@ -78,8 +89,38 @@ class TraitsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        Log.d("TraitsFragment", "TraitsFragment created.")
+        val mainAct =  (activity as MainActivity)
+        characterImage = mainAct.dataManager.traitsData.basicData.picture.toUri()
+        name = mainAct.dataManager.traitsData.basicData.name
+        characterClass = mainAct.dataManager.traitsData.basicData.characterClass
+        level = mainAct.dataManager.traitsData.basicData.level
+        race = mainAct.dataManager.traitsData.basicData.race
+        alignment = mainAct.dataManager.traitsData.basicData.alignment
+        personality = mainAct.dataManager.traitsData.complexData.personality
+        ideals = mainAct.dataManager.traitsData.complexData.ideals
+        bonds = mainAct.dataManager.traitsData.complexData.bonds
+        flaws = mainAct.dataManager.traitsData.complexData.flaws
+        background = mainAct.dataManager.traitsData.complexData.background
         //return super.onCreateView(inflater, container, savedInstanceState)
         return inflater.inflate(R.layout.fragment_traits, container, false)
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        val mainAct =  (activity as MainActivity)
+        mainAct.dataManager.traitsData.basicData.picture = characterImage.toString()
+        mainAct.dataManager.traitsData.basicData.name = name
+        mainAct.dataManager.traitsData.basicData.characterClass = characterClass
+        mainAct.dataManager.traitsData.basicData.level = level
+        mainAct.dataManager.traitsData.basicData.race = race
+        mainAct.dataManager.traitsData.basicData.alignment = alignment
+        mainAct.dataManager.traitsData.complexData.personality = personality
+        mainAct.dataManager.traitsData.complexData.ideals = ideals
+        mainAct.dataManager.traitsData.complexData.bonds = bonds
+        mainAct.dataManager.traitsData.complexData.flaws = flaws
+        mainAct.dataManager.traitsData.complexData.background = background
+        Log.d("TraitsFragment", "TraitsFragment destroyed.")
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -87,6 +128,179 @@ class TraitsFragment : Fragment() {
 
         val act = (activity as AppCompatActivity) // get activity
         act.supportActionBar?.title = "Character Name"
+
+        val image = view.findViewById<ImageView>(R.id.character_image)
+        image.setImageURI(characterImage)
+
+        val nameEditText = view.findViewById<EditText>(R.id.charcter_name_editText)
+        nameEditText.text = Editable.Factory.getInstance().newEditable(name)
+        nameEditText.addTextChangedListener(object :
+            TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                if (p0 != null) {
+                    name = p0.toString()
+                }
+            }
+        })
+
+        val classEditText = view.findViewById<EditText>(R.id.class_editText)
+        classEditText.text = Editable.Factory.getInstance().newEditable(characterClass)
+        classEditText.addTextChangedListener(object :
+            TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                if (p0 != null) {
+                    characterClass = p0.toString()
+                }
+            }
+        })
+
+        val levelEditText = view.findViewById<EditText>(R.id.level_editText)
+        levelEditText.text = Editable.Factory.getInstance().newEditable(level)
+        levelEditText.addTextChangedListener(object :
+            TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                if (p0 != null) {
+                    level = p0.toString()
+                }
+            }
+        })
+
+        val raceEditText = view.findViewById<EditText>(R.id.race_editText)
+        raceEditText.text = Editable.Factory.getInstance().newEditable(race)
+        raceEditText.addTextChangedListener(object :
+            TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                if (p0 != null) {
+                    race = p0.toString()
+                }
+            }
+        })
+
+        val alignmentEditText = view.findViewById<EditText>(R.id.alignment_editText)
+        alignmentEditText.text = Editable.Factory.getInstance().newEditable(alignment)
+        alignmentEditText.addTextChangedListener(object :
+            TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                if (p0 != null) {
+                    alignment = p0.toString()
+                }
+            }
+        })
+
+        val personalityEditText = view.findViewById<EditText>(R.id.personality_editText)
+        personalityEditText.text = Editable.Factory.getInstance().newEditable(personality)
+        personalityEditText.addTextChangedListener(object :
+            TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                if (p0 != null) {
+                    personality = p0.toString()
+                }
+            }
+        })
+
+        val idealsEditText = view.findViewById<EditText>(R.id.ideals_editText)
+        idealsEditText.text = Editable.Factory.getInstance().newEditable(ideals)
+        idealsEditText.addTextChangedListener(object :
+            TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                if (p0 != null) {
+                    ideals = p0.toString()
+                }
+            }
+        })
+
+        val bondsEditText = view.findViewById<EditText>(R.id.bonds_editText)
+        bondsEditText.text = Editable.Factory.getInstance().newEditable(bonds)
+        bondsEditText.addTextChangedListener(object :
+            TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                if (p0 != null) {
+                    bonds = p0.toString()
+                }
+            }
+        })
+
+        val flawsEditText = view.findViewById<EditText>(R.id.flaws_editText)
+        flawsEditText.text = Editable.Factory.getInstance().newEditable(flaws)
+        flawsEditText.addTextChangedListener(object :
+            TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                if (p0 != null) {
+                    flaws = p0.toString()
+                }
+            }
+        })
+
+        val backgroundEditText = view.findViewById<EditText>(R.id.background_editText)
+        backgroundEditText.text = Editable.Factory.getInstance().newEditable(background)
+        backgroundEditText.addTextChangedListener(object :
+            TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                if (p0 != null) {
+                    background = p0.toString()
+                }
+            }
+        })
 
         val characterImage = act.findViewById<ImageView>(R.id.character_image)
         characterImage.setOnClickListener() {
