153908e8b8ded80918dd6eee7482eecb32dd7b3a
Sawyer Knowles <sdknowle@mtu.edu>
2023-03-27 14:31:48 -0700

    Refactored save/load data to use internal storage rather than shared preferences. Small UI fix to notes list item, small UI update to character list item

--- app/src/main/java/com/example/ddcharacterapp/MainActivity.kt
+++ app/src/main/java/com/example/ddcharacterapp/MainActivity.kt
@@ -1,12 +1,12 @@
 package com.example.ddcharacterapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
 import androidx.activity.OnBackPressedCallback
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.MenuProvider
 import androidx.navigation.NavController
@@ -14,32 +14,57 @@ import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import java.io.*
 
 
 class MainActivity : AppCompatActivity(), MenuProvider {
+
+    val FILE_NAME = "DDCharAppData.json"
 
     var characterListGlobal = ArrayList<CharacterCardData>() // Global List of Character Data
     var characterIndex = 0 // Global index in characterListGlobal for currently selected character
     var dataManager = DataManager() // Global DM container - set to Character specific DM in CharacterCardAdapter
 
     private fun saveData() {
-        val sharedPreferences = getSharedPreferences("shared preferences", MODE_PRIVATE)
-        val editor = sharedPreferences.edit()
+
         val gson = Gson()
         val json = gson.toJson(characterListGlobal)
-        editor.putString("character list", json)
-        editor.apply()
+
+        val fileOutputStream:FileOutputStream
+        try {
+            fileOutputStream = openFileOutput(FILE_NAME, MODE_PRIVATE)
+            fileOutputStream.write(json.toByteArray())
+        } catch (e: FileNotFoundException){
+            e.printStackTrace()
+        }catch (e: NumberFormatException){
+            e.printStackTrace()
+        }catch (e: IOException){
+            e.printStackTrace()
+        }catch (e: Exception){
+            e.printStackTrace()
+        }
+
     }
 
     private fun loadData() {
-        val sharedPreferences = getSharedPreferences("shared preferences", MODE_PRIVATE)
+
+        val file = this.getFileStreamPath(FILE_NAME)
+        if (file == null || !file.exists()) {
+            return
+        }
+
         val gson = Gson()
-        val json = sharedPreferences.getString("character list", null)
+        var fileInputStream: FileInputStream? = null
+        fileInputStream = openFileInput(FILE_NAME)
+        var inputStreamReader = InputStreamReader(fileInputStream)
+        val bufferedReader = BufferedReader(inputStreamReader)
+        val json = bufferedReader.use { it.readText() }
         val type = object : TypeToken<ArrayList<CharacterCardData>>() {}.type
         if(json == null) {
             return
         }
         characterListGlobal = gson.fromJson(json, type)
+
     }
 
     private val onBackPressedCallback = object : OnBackPressedCallback(true) {
